92482b2e96fe76bdbcad27a119bd7e58a2911310
Joan Teixidó <joan.teixido@bismart.com>
2023-03-27 23:08:32 +0200

    change size customer

--- dbo/Tables/Customer.sql
+++ dbo/Tables/Customer.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Customer]
 (
 	[Id] INT NOT NULL PRIMARY KEY,
-	[Name] VARCHAR(100) NOT NULL,
+	[Name] VARCHAR(150) NOT NULL,
 	[Email] Varchar(200) NOT NULL,
 	[Phone] varchar(50) NULL
 )
