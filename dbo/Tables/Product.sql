a982ee3fc7c73e85f8328c9e583ddf4bb41f1da2
Joan Teixidó <joan.teixido@bismart.com>
2023-03-27 22:11:22 +0200

    moving tables

--- dbo/Tables/Product.sql
+++ dbo/Tables/Product.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Product]
 (
-	[Id] INT NOT NULL PRIMARY KEY,
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
 	[Name] VARCHAR(255) NOT NULL, 
 	[Description] VARCHAR(max) NULL,
 	[Price] Decimal(16, 2)
